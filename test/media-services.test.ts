d2a957da42ee9153fe280d2c8a38ee9e0ac0f834
starfrogsplash <a.khal786@outlook.com>
2024-06-21 12:02:24 +0100

    Initial commit

---
+++ test/media-services.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MediaServices from '../lib/media-services-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/media-services-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MediaServices.MediaServicesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
